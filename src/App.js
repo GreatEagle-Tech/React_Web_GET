d37e9a972dfbc43f8444ba7807ee6eb32da34b68
RoboWorld <singh.raj1997@gmail.com>
2022-05-27 23:27:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import classes from "./App.module.css";
+import Header from "./components/Header";
+import Home from "./components/pages/Home";
+import Honours from "./components/pages/Honours";
+import Seminars from "./components/pages/Seminars";
+
+function App() {
+  return (
+    <div className={classes.body}>
+      <Header />
+      <main className={classes.main} id="idMain">
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/honours" element={<Honours />}></Route>
+          <Route path="/seminars" element={<Seminars />}></Route>
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import classes from "./Header.module.css";
+import droneIcon from "../assets/drone-icon.png";
+
+const Header = () => {
+  const [headClass, setHeadClass] = useState(classes.navbar);
+
+  const changeBackground = () => {
+    if (element.scrollTop === 0) {
+      setHeadClass(classes["header"]);
+    } else {
+      setHeadClass(classes["header-active"]);
+    }
+  };
+
+  let element = document.getElementById("idMain"); // check useEffects
+  console.log(element);
+
+  if (element) element.addEventListener("scroll", changeBackground);
+
+  return (
+    <header className={classes.header}>
+      <nav className={headClass}>
+        <img src={droneIcon} height={30} width={75} />
+        <ul>
+          <li>
+            <NavLink
+              className={(navData) => (navData.isActive ? classes.active : "")}
+              to="/home"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) => (navData.isActive ? classes.active : "")}
+              to="/products"
+            >
+              Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) => (navData.isActive ? classes.active : "")}
+              to="/resourses"
+            >
+              Resourses
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className={(navData) => (navData.isActive ? classes.active : "")}
+              to="/about-us"
+            >
+              About Us
+            </NavLink>
+          </li>
+        </ul>
+        <button className={classes.btn}> Contact Us</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
